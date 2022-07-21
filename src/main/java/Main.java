2baeaea2387e86410021b2c93965dfb8b2efb607
Caroline Rives <car.rives@gmail.com>
2022-07-21 14:47:18 -0400

    Collectors lab complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,27 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Main {
+    /*
+    Write a method called multiplySquaresOfNumbers that takes in a List<Integer> and returns the
+    product of squares of the integer numbers as a long.
+    Use the reducing method in the Collectors class for this problem.
+     */
 
     // modify this method
     public static long multiplySquaresOfNumbers(List<Integer> numbers) {
-        // your code here
-        return 0;
+        long l = numbers
+                    .stream()
+                    .map(i -> i * i)
+                    .collect(Collectors.reducing((runningTotal, currentStreamValue)
+                            -> runningTotal * currentStreamValue))
+                    .stream()
+                .mapToLong(Integer::longValue)
+                .findAny()
+                .getAsLong();
+        return l;
     }
 
     public static void main(String[] args) {
